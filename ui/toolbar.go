67e1f8b679cd35ed2de63d96815e92465369e7a1
Mark Essien <markessien@gmail.com>
2022-09-11 13:55:34 +0200

    Fixed issue with btn icons not working on linux

--- ui/toolbar.go
+++ ui/toolbar.go
@@ -3,7 +3,21 @@ package ui
 import (
 	"log"
 
+	_ "embed"
+
+	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/gtk"
+)
+
+//go:embed icons/connect.png
+var imgConnectBuf []byte
+
+//go:embed icons/disconnect.png
+var imgDisconnectBuf []byte
+
+var (
+	imageConnect    *gdk.Pixbuf = nil
+	imageDisconnect *gdk.Pixbuf = nil
 )
 
 type Toolbar struct {
@@ -18,11 +32,25 @@ func (tb *Toolbar) Create() *gtk.Toolbar {
 	}
 	toolbar.SetStyle(gtk.TOOLBAR_BOTH)
 
+	imageConnect, err = gdk.PixbufNewFromBytesOnly(imgConnectBuf)
+	if err != nil {
+		log.Fatal("Unable to load image:", err)
+	}
+	imageConnect, _ = imageConnect.ScaleSimple(16, 16, gdk.INTERP_BILINEAR)
+
+	imageDisconnect, err = gdk.PixbufNewFromBytesOnly(imgDisconnectBuf)
+	if err != nil {
+		log.Fatal("Unable to load image:", err)
+	}
+	imageDisconnect, _ = imageDisconnect.ScaleSimple(16, 16, gdk.INTERP_BILINEAR)
+
 	img, err := gtk.ImageNew()
 	if err != nil {
 		log.Fatal("Unable to create image:", err)
 	}
-	img.SetFromIconName("media-playback-start", gtk.ICON_SIZE_BUTTON)
+	img.SetFromPixbuf(imageConnect)
+
+	// .SetFromIconName("media-playback-start", gtk.ICON_SIZE_BUTTON)
 	playTb, err := gtk.ToolButtonNew(img, "Connect")
 	if err != nil {
 		log.Fatal("Unable to create newTb:", err)
@@ -31,7 +59,8 @@ func (tb *Toolbar) Create() *gtk.Toolbar {
 	toolbar.Insert(playTb, -1)
 
 	img, _ = gtk.ImageNew()
-	img.SetFromIconName("media-playback-stop", gtk.ICON_SIZE_BUTTON)
+	// img.SetFromIconName("media-playback-stop", gtk.ICON_SIZE_BUTTON)
+	img.SetFromPixbuf(imageDisconnect)
 	stopTb, err := gtk.ToolButtonNew(img, "Disconnect")
 	if err != nil {
 		log.Fatal("Unable to create newTb:", err)
