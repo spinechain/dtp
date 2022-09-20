89d288459f00ebdf4c828e9f9af38c9e7f5494ae
Mark Essien <markessien@gmail.com>
2022-09-20 14:25:45 +0200

    fixed the default peer loading so we can manually specify peers

--- util/util.go
+++ util/util.go
@@ -1,6 +1,9 @@
 package util
 
-import "os"
+import (
+	"bufio"
+	"os"
+)
 
 func FileExists(filename string) bool {
 	info, err := os.Stat(filename)
@@ -8,4 +11,21 @@ func FileExists(filename string) bool {
 		return false
 	}
 	return !info.IsDir()
+}
+
+// Read file utility function
+func ReadFile(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	fileInfo, _ := file.Stat()
+	var size int64 = fileInfo.Size()
+	bin := make([]byte, size)
+	buffer := bufio.NewReader(file)
+	_, err = buffer.Read(bin)
+
+	return string(bin), err
 }
