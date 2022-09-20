2909bd77966796395c486c343d37be76060a629e
Mark Essien <markessien@gmail.com>
2022-09-20 14:58:20 +0200

    added ping task type

---
+++ tasktypes/ping.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Welcome
+echo "Linux: Welcome to the Ping tasktype!"
+
+# Get the first script parameter
+TARGET=$1
+OUTPUT_DIR=$2
+OUT_FILE="$OUTPUT_DIR/output.txt"
+
+echo "Pinging: $TARGET to $OUTPUT_DIR"
+
+ping $TARGET -n 4 > $OUT_FILE
+
+echo "done"
