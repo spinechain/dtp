57fcf7490c64c2752f4f58e3a5296f36eedf02fc
Mark Essien <markessien@gmail.com>
2022-09-21 13:33:11 +0200

    added build script

---
+++ build.bat
@@ -0,0 +1,10 @@
+echo "Building SpineChain"
+
+set VERSION=0.0.1
+
+set GOOS=windows
+go build -o package/windows/spinechain-%VERSION%.exe -ldflags "-X main.version=%VERSION%" .
+
+set GOOS=linux
+
+go build -o package/linux/spinechain -ldflags "-X main.version=%VERSION%" .
