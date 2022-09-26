ae8f66e84c54423da2c61f9e92d7e95d07fc7e37
Mark Essien <markessien@gmail.com>
2022-09-26 12:24:56 +0200

    routing improved

--- tasknet/forward.go
+++ tasknet/forward.go
@@ -4,6 +4,38 @@ import "spinedtp/util"
 
 // This file contains the routines that forward our packages to other peers
 
+func RouteTaskOn(task *Task) {
+
+	util.PrintBlue("Routing Task On: " + task.ID + " (" + task.Command + ") ")
+
+	// update status so we never deal with this task again
+	OpenTaskPool.UpdateTaskStatus(task, task.GlobalStatus, StatusRoutedToNetwork, task.LocalWorkProviderStatus)
+	task.MarkAsPropagated(OpenTaskPool)
+
+	// We send to clients, except clients that were already on route or the task owner
+	for _, peer := range Peers {
+
+		// Check if this peer is in the arrival route
+		alreadyOnRoute := false
+		for _, routePeer := range task.ArrivalRoute {
+			if routePeer.ID == peer.ID {
+				alreadyOnRoute = true
+				break
+			}
+		}
+
+		if peer.ID != task.TaskOwnerID && !alreadyOnRoute && peer.Connected {
+
+			packet, err := ConstructTaskPropagationPacket(task)
+			if err != nil {
+				continue
+			}
+
+			peer.SendPacket(packet)
+		}
+	}
+}
+
 func RouteTaskBidOn(tb *TaskBid) {
 	util.PrintBlue("Routing TaskBid On: " + tb.ID + " (TaskID:" + tb.TaskID + ") ")
 
