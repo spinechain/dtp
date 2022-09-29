690d3cf4258bb24c4b751d0bb96606da44883954
Mark Essien <markessien@gmail.com>
2022-09-29 15:23:42 +0200

    Improvements to the build and publish for ubuntu

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ set VERSION=0.0.1
 set GOOS=windows
 go build -o package/windows/spinechain-%VERSION%.exe -ldflags "-X main.version=%VERSION%" .
 
-gh release delete v%VERSION%
+gh release delete v%VERSION% -y
 gh release create v%VERSION% --title "v%VERSION%" --generate-notes
 gh release upload v%VERSION% .\package\windows\spinechain-%VERSION%.exe
--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-VERSION=$(<VERSION)
-echo "$value"
 
+VERSION=$(<VERSION)
 echo "Building SpineChain v$VERSION..."
 
 set GOOS=linux
 go build -o package/linux/spinechain-$VERSION -ldflags "-X main.version=$VERSION" .
+
+gh release delete-asset v$VERSION spinechain-$VERSION
+gh release upload v$VERSION ./package/linux/spinechain-$VERSION#"spinechain-$VERSION - Ubuntu Package"
