4fe535f95d7530d262c4a28099b6cf9c674e3751
Mark Essien <markessien@gmail.com>
2022-09-29 15:27:36 +0200

    Added dynamic versioning to the windows package

--- build.bat
+++ build.bat
@@ -1,9 +1,10 @@
 echo "Building SpineChain..."
 
-set VERSION=0.0.1
+set /p VERSION=<VERSION
+
 set GOOS=windows
 go build -o package/windows/spinechain-%VERSION%.exe -ldflags "-X main.version=%VERSION%" .
 
-gh release delete v%VERSION% -y
+:: gh release delete v%VERSION% -y
 gh release create v%VERSION% --title "v%VERSION%" --generate-notes
-gh release upload v%VERSION% .\package\windows\spinechain-%VERSION%.exe
+gh release upload v%VERSION% .\package\windows\spinechain-%VERSION%.exe#"spinechain-%VERSION%.exe - Windows 64bit"
