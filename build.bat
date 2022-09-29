9749985cae365ccc433ab5db7b28f5c293183bc2
Mark Essien <markessien@gmail.com>
2022-09-29 15:05:07 +0200

    Added release bat file for windows

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
 echo "Building SpineChain..."
 
+set VERSION=0.0.1
 set GOOS=windows
-go build -o package/spinechain.exe -ldflags "-X main.version=%VERSION%" .
+go build -o package/windows/spinechain-%VERSION%.exe -ldflags "-X main.version=%VERSION%" .
+
+gh release delete v%VERSION%
+gh release create v%VERSION% --title "v%VERSION%" --generate-notes
+gh release upload v%VERSION% .\package\windows\spinechain-%VERSION%.exe
